e63c2bec7150185cb845dab005e0e0a7dd93fbea
shreyvyas <shreyvyas@gmail.com>
2026-02-20 00:30:39 +0530

    Add Variables.py with basic data types and examples

---
+++ Basics/Variables.py
@@ -0,0 +1,24 @@
+a = 10                                         #integer
+print("Integer: ", a)
+
+b = 20.1                                       #float
+print("Float: ", b)
+
+str = "shrey"                                  #string
+print(str)
+
+list = [1,2,3,"Shrey", 10.1]                   #list: mutable, ordered, mixed datatype, allows duplicates
+print(list)
+
+tup = (1,2,3,4,5,6,"shrey")                   #tup: immutable, ordered
+print(tup)
+
+set1 = {10, True, 90.5, "shrey"}              #set: duplicates are not allowed, not ordered
+print(set1)
+
+dict1 = {"name":"shrey", "age": 36, "city":"bengaluru"}      #key-value pair, used in test automation
+print(dict1.keys())
+print(dict1.values())
+
+value = False
+print(value)
