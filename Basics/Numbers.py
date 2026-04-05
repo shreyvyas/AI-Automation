2ac6f84d8d01b57c50c075a1171c16ea08e65d3f
shreyvyas <shreyvyas@gmail.com>
2026-04-05 23:06:47 +0530

    Add Numbers.py with examples of integer, boolean, float, string, and character types

---
+++ Basics/Numbers.py
@@ -0,0 +1,18 @@
+#Python Numbers
+
+a = 10
+print(a)
+print(type (a))
+
+is_done = True
+print(is_done)
+print(type(is_done))
+
+float1 = 10.5
+print(type(float1))
+
+name1 = "Shrey";
+print(type(name1))
+
+char1 = 'j'
+print(type(char1))
